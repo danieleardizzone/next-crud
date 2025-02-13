ee04eb7d7ca56e441f7f30cd04ff6df473dc0981
danieleardizzone <daniele.ardi02@gmail.com>
2025-02-13 20:50:37 +0100

    ADD - dropdown

--- app/components/DropDown.tsx
+++ app/components/DropDown.tsx
@@ -1,25 +1,39 @@
 'use client';
+
+import Link from "next/link";
+
 export default function DropDown() {
+
+    function toggleDropdown() {
+        const dropDownMenu = document.querySelector('#dropDownMenu');
+        dropDownMenu?.classList.toggle('hidden');
+    }
+
     return (
         <>
             <button
+                onClick={toggleDropdown}
                 className="relative py-1 px-5 w-22 sm:w-28 text-md sm:text-lg font-bold rounded-2xl border-2 border-btn bg-btn"
             >
-                Users
+                <div className="flex items-center gap-2">
+                    <span>Users</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                    </svg>
+
+
+                </div>
             </button>
-            {/* <div className="absolute w-48">
-                <div className="bg-lavender sm:text-lg backdrop-blur-lg rounded-xl border border-gray-700 divide-y divide-gray-700">
+            <div className="w-44 absolute top-[76px] hidden" id="dropDownMenu">
+                <div className="sm:text-lg rounded-xl shadow-lg bg-card border border-card divide-y divide-card">
                     <div className="py-2 px-3">
-                        link 1
+                        <Link href="/users">List</Link>
                     </div>
                     <div className="py-2 px-3">
-                        link 1
-                    </div>
-                    <div className="py-2 px-3">
-                        link 1
+                        <Link href="/users/create">Create</Link>
                     </div>
                 </div>
-            </div> */}
+            </div>
         </>
     )
 }
